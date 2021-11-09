0108a6588e39cb9aea8795c84a8436266d3fb184
dey-z <anish.13dey@gmail.com>
2021-11-09 18:19:30 +0900

    remove: Scanf

--- monk_and_rotation/cmd/main.go
+++ monk_and_rotation/cmd/main.go
@@ -1,29 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	var T int
 	fmt.Scanf("%d", &T)
-	for T != 0 {
-		var N, K int
-		fmt.Scanf("%d %d", &N, &K)
-		arr := make([]int, N)
-		buf := make([]interface{}, N)
-		for i := 0; i < N; i++ {
-			buf[i] = &arr[i]
+	scanner := bufio.NewScanner(os.Stdin)
+	const maxCapacity = 1000000
+	buf := make([]byte, maxCapacity)
+	scanner.Buffer(buf, maxCapacity)
+
+	cnt := 0
+	var N, K int
+	for scanner.Scan() {
+		var line string
+		line = scanner.Text()
+		if cnt%2 != 0 {
+			nums := strings.Fields(line)
+			index := N - (K % N)
+			fmt.Print(strings.Trim(fmt.Sprint(nums[index:N]), "[]"))
+			if len(nums[index:N]) > 0 && len(nums[0:index]) > 0 {
+				fmt.Print(" ")
+			}
+			fmt.Print(strings.Trim(fmt.Sprint(nums[0:index]), "[]"))
+			fmt.Println("")
+		} else {
+			N, _ = strconv.Atoi(strings.Split(line, " ")[0])
+			K, _ = strconv.Atoi(strings.Split(line, " ")[1])
 		}
-		fmt.Scanln(buf...)
-		index := N - (K % N)
-		fmt.Print(strings.Trim(fmt.Sprint(arr[index:N]), "[]"))
-		if len(arr[index:N]) > 0 && len(arr[0:index]) > 0 {
-			fmt.Print(" ")
+		cnt += 1
+		if cnt >= T*2 {
+			break
 		}
-		fmt.Print(strings.Trim(fmt.Sprint(arr[0:index]), "[]"))
-		fmt.Println("")
-		T -= 1
 	}
 }
