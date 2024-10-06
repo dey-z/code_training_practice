450b58b24aec8cd38e70262c8210f93dcdf2ee1c
dey-z <anish.13dey@gmail.com>
2024-10-06 23:12:49 +0900

    update fib_frog

--- codility/fib_frog/fib_frog.go
+++ codility/fib_frog/fib_frog.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"os"
 
 	"github.com/dey-z/code_training_practice/codility/fib_frog/testdata"
@@ -19,74 +18,41 @@ func Solution(A []int) int {
 	// add last leaf so as to assign target bank
 	A = append(A, 1)
 	N := len(A)
-	// find Fibonacci list
-	var F []int
-	fibCached := make(map[int]bool)
-	// F[0] = 0
-	F = append(F, 0)
-	// F[1] = 1
-	F = append(F, 1)
-	// first find fibonacci upto F[2:K] < N
-	i := 2
-	for i < math.MaxInt32 {
-		fib := F[i-1] + F[i-2]
-		if fib > N {
-			break
-		}
-		F = append(F, fib)
-		fibCached[fib] = true
-		i += 1
-	}
-	// not considering 1st two fibs which are 0 & 1
-	F = F[2:]
-	// algorithm
-	// 1. get starting leaves that can be reached in one step
-	// 2. then find intermediate leaves
-	// 3. find min of combinations of 2.
 
-	// initialize reachSteps with 0
+	// find Fibonacci list
+	F := []int{0, 1}
+	for i := 2; F[i-1]+F[i-2] <= N; i++ {
+		F = append(F, F[i-1]+F[i-2])
+	}
+
+	// initialize reachSteps with -1
 	reachSteps := make([]int, N)
 	for i := range reachSteps {
-		reachSteps[i] = 0
+		reachSteps[i] = -1
 	}
 
-	// leafs that can be reached in one fibonacci step
-	i = 0
-	for i < N {
-		if A[i] == 1 && fibCached[i+1] {
-			reachSteps[i] = 1
+	// find the minimum number of jumps to each position
+	for i := 0; i < len(A); i++ {
+		if A[i] == 1 || i == len(A)-1 {
+			minStep := -1
+			for _, fib := range F {
+				prevPos := i - fib
+				if prevPos < -1 {
+					break
+				}
+				if prevPos == -1 {
+					minStep = 1
+					break
+				}
+				if reachSteps[prevPos] > 0 {
+					if minStep == -1 || reachSteps[prevPos]+1 < minStep {
+						minStep = reachSteps[prevPos] + 1
+					}
+				}
+			}
+			reachSteps[i] = minStep
 		}
-		i += 1
 	}
 
-	// search leafs with more than one step
-	i = 0
-	for i < N {
-		if A[i] == 0 || reachSteps[i] > 0 {
-			i += 1
-			continue
-		}
-		minI := -1
-		minV := 100000
-		for _, fib := range F {
-			previousI := i - fib
-			if previousI < 0 {
-				break
-			}
-			if reachSteps[previousI] > 0 && minV > reachSteps[previousI] {
-				minV = reachSteps[previousI]
-				minI = previousI
-			}
-			if minI != -1 {
-				reachSteps[i] = minV + 1
-			}
-		}
-		i += 1
-	}
-
-	if reachSteps[N-1] > 0 {
-		return reachSteps[N-1]
-	} else {
-		return -1
-	}
+	return reachSteps[N-1]
 }
